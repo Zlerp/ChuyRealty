fd71f1270e363c1bf5f69b34d9748b87930e80cc
Zlerp <zhpdeals@gmail.com>
2015-08-26 17:38:03 -0400

    working Gallery

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -7,7 +7,7 @@ $(function() {    // same as $( document ).ready(function() {
 
   var width = 720;
   var animationSpeed = 1000;
-  var pause = 2000;
+  var pause = 5000;
   var currentSlide = 1;
 
   // cache DOM  --> Only have to search page once.
@@ -39,6 +39,30 @@ $(function() {    // same as $( document ).ready(function() {
   $slider.on('mouseenter', pauseSlider).on('mouseleave', startSlider);
 
   startSlider();
+
+  $( "#next" ).click(function() {
+    $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+      currentSlide++;
+      if (currentSlide === $slides.length) {
+        currentSlide = 1;
+        $slideContainer.css('margin-left', 0);
+        }
+    }, 0);
+  });
+
+  $( "#prev" ).click(function() {
+    // if (currentSlide > 1) {
+    $slideContainer.animate({'margin-left': '+='+width}, animationSpeed, function() {
+      currentSlide--;
+      console.log(currentSlide);
+      if (currentSlide === 0) {
+        console.log("working");
+        currentSlide = $slides.length;
+        $slideContainer.css('margin-left', 4560);
+        }
+      }, 0);
+    // }
+  });
 
     //setInterval
     //animate
