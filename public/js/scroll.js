3d4974159734fcb9d272959775188d1e08518abc
Zlerp <zhpdeals@gmail.com>
2015-09-02 21:04:30 -0400

    CSS ADD

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -67,8 +67,6 @@ $(function() {    // same as $( document ).ready(function() {
 
   var mobCount = 1;
 
-
-
   $("section").mouseup(function(e)  {
     var subject = $(".menu");
       if(e.target.id != subject.attr('id')) {
@@ -107,4 +105,57 @@ $( "#menuBurger" ).click(function(evt) {
       });
 
 
+  // GOOGLE MAPS
+
+
+
+  var map;
+  var markers;
+  var mapName = 'CHUY REALTY GROUP';
+
+  var contentString =
+  '<p>'+ mapName + '</p>'
+  // +
+  // '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+  // 'sandstone rock formation in the southern part of the '+
+  // 'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+  // 'south west of the nearest large town, Alice Springs; 450&#160;km '+
+  // '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+  // 'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+  // 'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+  // 'Aboriginal people of the area. It has many springs, waterholes, '+
+  // 'rock caves and ancient paintings. Uluru is listed as a World '+
+  // 'Heritage Site.</p>'+
+  // '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+  // 'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+  // '(last visited June 22, 2009).</p>'
+  ;
+
+var marker;
+
+  function initMap() {
+    var myLatLng = {lat: 35.261264, lng: -79.025162};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom:8,
+      center: myLatLng
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Hello World!'
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+  }
+
+
+google.maps.event.addDomListener(window, 'load', initMap);
+marker.setMap(map);
 });
